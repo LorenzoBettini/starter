12900b2b574cb2bbe1be3142228985e75ae53383
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2024-11-15 16:05:59 +0100

    in-and-out map to Alt+ENTER
    Because CTRL+ENTER is just ENTER in Gnome Terminal

--- lua/plugins/in-and-out.lua
+++ lua/plugins/in-and-out.lua
@@ -2,7 +2,10 @@ return {
   "ysmb-wtsg/in-and-out.nvim",
   keys = {
     {
-      "<C-CR>",
+      -- the suggested keybinding Ctrl+ENTER, does not work in Gnome Terminal
+      -- because Ctrl-ENTER in Gnome Terminal is just ENTER,
+      -- so let's map it to "Alt+ENTER"
+      "<a-CR>",
       function()
         require("in-and-out").in_and_out()
       end,
