abf2afededd1dd4e58b345dfe5403e937c8846c4
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2024-12-15 18:47:50 +0100

    use Telescope as picker

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -6,3 +6,7 @@ vim.opt.winbar = "%=%m %f"
 vim.opt.expandtab = false -- Use spaces instead of tabs
 
 vim.opt.scrolloff = 999 -- Lines of context; high value keep the cursor centered vertically in the screen
+
+-- In case you don't want to use `:LazyExtras`,
+-- then you need to set the option below.
+vim.g.lazyvim_picker = "telescope"
