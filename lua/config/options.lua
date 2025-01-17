1c28b9ba2e4e4c76fbfd8412d5d4e0cf310e450f
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2025-01-17 19:19:19 +0100

    try scrolloff 7

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -6,7 +6,7 @@ vim.opt.winbar = "%=%m %f"
 -- set it on a per file basis
 -- vim.opt.expandtab = false -- Use spaces instead of tabs
 
-vim.opt.scrolloff = 999 -- Lines of context; high value keep the cursor centered vertically in the screen
+vim.opt.scrolloff = 7 -- Lines of context; high value keep the cursor centered vertically in the screen
 
 -- In case you don't want to use `:LazyExtras`,
 -- then you need to set the option below.
