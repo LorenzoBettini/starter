173b66819cc48f7d534a3f79f95dc2bffb2c2994
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2025-01-31 11:16:30 +0100

    automatically re-generate the spl file when add file has changed

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,24 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- Ensure that the binary spl file is up-to-date with the source add file
+vim.api.nvim_create_autocmd("FocusGained", {
+  pattern = "*",
+  callback = function()
+    local config_path = vim.fn.stdpath("config") -- Get Neovim's config path
+    local add_file = config_path .. "/spell/en.utf-8.add"
+    local spl_file = config_path .. "/spell/en.utf-8.add.spl"
+
+    -- Check if both files exist
+    if vim.fn.filereadable(add_file) == 1 then
+      local add_mtime = vim.fn.getftime(add_file) -- Get modification time of .add file
+      local spl_mtime = vim.fn.getftime(spl_file) -- Get modification time of .add.spl file
+
+      -- Run mkspell! only if .add is newer than .add.spl or .add.spl doesn't exist
+      if add_mtime > spl_mtime or spl_mtime == -1 then
+        vim.cmd("silent! mkspell! " .. spl_file .. " " .. add_file)
+      end
+    end
+  end,
+})
