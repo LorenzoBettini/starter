fc5b7e9d3d31069837827d4280874f5a52915493
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2026-05-11 12:40:30 +0200

    texlablabel extra definition
    To deal with a current problem in texlab that does not recognize labels
    defined inside lstlistings

--- lua/plugins/extend-lspconfig.lua
+++ lua/plugins/extend-lspconfig.lua
@@ -1,6 +1,4 @@
--- since this is just an example spec, don't actually load anything here and return an empty spec
--- stylua: ignore
--- if true then return {} end
+-- Extend the default configuration for lspconfig. This is useful for adding additional servers or customizing existing ones without modifying the default configuration directly.
 return {
   {
     "neovim/nvim-lspconfig",
@@ -11,11 +9,21 @@ return {
       local s = opts.servers.texlab.settings or {}
       local t = s.texlab or {}
       local d = t.diagnostics or {}
+      local e = t.experimental or {}
 
+      -- Customize diagnostics to ignore specific patterns, such as "Underfull \hbox" warnings that are common in LaTeX documents and may not always indicate a problem.
       d.ignoredPatterns = d.ignoredPatterns or {}
       table.insert(d.ignoredPatterns, [[Underfull \\hbox]])
 
+      -- Add experimental features to the texlab configuration, such as custom label definition commands.
+      -- This allows the language server to recognize and provide features for custom LaTeX commands that define labels, improving the development experience when working with LaTeX documents.
+      -- In particular, this mitigates https://github.com/latex-lsp/texlab/issues/1159 (label definitions in lstlistings environments not recognized) by adding "texlablabel" as a label definition command.
+      -- You must define this in the preamble "\newcommand{\texlablabel}[1]{}" and then write "\texlablabel{mylabel}" near each lstlistings environment using label={mylabel} to have the label recognized by texlab.
+      e.labelDefinitionCommands = e.labelDefinitionCommands or {}
+      table.insert(e.labelDefinitionCommands, "texlablabel")
+
       t.diagnostics = d
+      t.experimental = e
       s.texlab = t
       opts.servers.texlab.settings = s
     end,
