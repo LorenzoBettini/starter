197f63e0727509c7f9c669a2cb25c6dd094c5fc8
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2026-07-22 19:09:20 +0200

    removed resume additional keybinding (already sR)

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,11 +1,13 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-vim.keymap.set(
-  "n",
-  "<leader>sx",
-  require("telescope.builtin").resume,
-  { noremap = true, silent = true, desc = "Resume" }
-)
+
+-- Already bound by LazyVim to sR
+-- vim.keymap.set(
+--   "n",
+--   "<leader>sx",
+--   require("telescope.builtin").resume,
+--   { noremap = true, silent = true, desc = "Resume" }
+-- )
 
 vim.keymap.set("x", "<leader>p", [["_dP]], { noremap = true, silent = true, desc = "Paste keeping yanked" })
